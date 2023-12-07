ef43a642554e630ff008fed009d4fd4f8e37ba90
Alan Bubalo <alan.buba5@gmail.com>
2023-12-07 01:07:22 +0100

    Remove domain & path in cookie

--- backend/src/services/cookieService.js
+++ backend/src/services/cookieService.js
@@ -14,8 +14,6 @@ export const getCookieTokenFromReq = (req) => {
 export const addAuthCookieToRes = (res, accessToken) => {
     res.cookie(COOKIE_NAME, accessToken, {
         expires: now().add(1, 'year').toDate(),
-        domain: process.env.FRONTEND_DOMAIN,
-        path: '/',
         httpOnly: true,
         secure: true,
         sameSite: 'none',
