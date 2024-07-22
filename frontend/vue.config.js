11ba3d9e2b5fc24c42344a3f68baace265ca2b2c
AlphaActual <tin.priti@gmail.com>
2024-07-22 22:51:51 +0200

    test

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,8 +1,14 @@
 const { defineConfig } = require('@vue/cli-service');
 module.exports = defineConfig({
     transpileDependencies: true,
-
-    devServer: {
-        disableHostCheck: true,
+    allowedHosts: [
+        'localhost',
+        'sou-program-app-preview-tin-front.onrender.com',
+        'https://sou-program-app-preview-tin.onrender.com',
+    ],
+    headers: {
+        'Access-Control-Allow-Origin': '*',
     },
 });
+
+// { allowedHosts?, bonjour?, client?, compress?, devMiddleware?, headers?, historyApiFallback?, host?, hot?, http2?, https?, ipc?, liveReload?, magicHtml?, onAfterSetupMiddleware?, onBeforeSetupMiddleware?, onListening?, open?, port?, proxy?, server?, setupExitSignals?, setupMiddlewares?, static?, watchFiles?, webSocketServer? }
