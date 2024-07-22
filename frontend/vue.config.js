d673ffdc2067186c6a5be12d9dab1a93efc6fc25
AlphaActual <tin.priti@gmail.com>
2024-07-22 22:25:49 +0200

    chore: Remove devServer configuration from vue.config.js

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,8 +1,4 @@
 const { defineConfig } = require('@vue/cli-service');
 module.exports = defineConfig({
     transpileDependencies: true,
-    devServer: {
-        allowedHosts: 'all',
-        port: 8080,
-    },
 });
