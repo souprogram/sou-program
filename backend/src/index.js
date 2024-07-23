82c0773f10df1d1499f9c20800003db49cb6eb7e
Tin Pritisanac <tin.priti@gmail.com>
2024-07-23 08:56:27 +0200

    chore: Update CORS origin to include http://localhost:8080

--- backend/src/index.js
+++ backend/src/index.js
@@ -36,7 +36,7 @@ app.use(cookieParser());
 app.use(
     cors({
         credentials: true,
-        origin: [process.env.FRONTEND_URL, 'http://localhost:3000'],
+        origin: [process.env.FRONTEND_URL, 'http://localhost:8080'],
         transports: ['websocket'],
     })
 );
